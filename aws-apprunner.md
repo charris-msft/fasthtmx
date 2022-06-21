b267a8d8585684605c2cefe331c0e0ec5931f526
Chris Harris <charris@microsoft.com>
2022-06-21 11:28:44 -0700

    Add Dockerfile and AWS AppRunner files

---
+++ aws-apprunner.md
@@ -0,0 +1,126 @@
+# Instructions for deploying custom docker image to AWS App Runner
+
+## 2-Install the pre-requisites
+
+- [Install the AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html)
+- or use `winget`
+
+    ```bash
+    winget install Amazon.AWSCLI
+    ```
+
+## 3-Create and Deploy
+
+### Create the IAM role as specified in the json file
+
+`Role Creation DOESN'T WORK YET - RELYING ON EXISTING ROLE CREATED PREVIOUSLY SOMEHOW!`
+
+- "AccessRoleArn": "arn:aws:iam::500805876079:role/AppRunnerECRAccess"
+- `aws iam create-role --role-name AppRunnerECRAccess --assume-role-policy-document file://apprunner-role.json --assume-role-policy-document ./aws-apprunner-ecr-policy.json`
+- Follow [these instructions](https://aws.amazon.com/premiumsupport/knowledge-center/ecs-tasks-pull-images-ecr-repository/#:~:text=For%20AWS%20Fargate%20launch%20types%2C%20you%20must%20grant,role%20type%2C%20and%20then%20choose%20Elastic%20Container%20Service.)
+
+### Publish the Docker image to ECR
+
+- Login to ECR (if necessary)
+
+    ```bash
+    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 500805876079.dkr.ecr.us-east-1.amazonaws.com
+    ```
+
+- Tag the image
+
+    ```bash
+    docker tag fasthtmx 500805876079.dkr.ecr.us-east-1.amazonaws.com/charris/fasthtmx:latest
+    ```
+
+- Push the image to ECR
+
+    ```bash
+    docker push 500805876079.dkr.ecr.us-east-1.amazonaws.com/charris/fasthtmx:latest
+    ```
+
+### Create the App Runner service
+
+- Create a file named `aws-apprunner.json` with the configuration of the service you are creating
+  - Specify your `ServiceName`
+  - Specify the `AccessRoleArn` (you can get this from IAM in the AWS console)
+  - Specify the `ImageIdentifier` for the image you published to ECR in the previous step
+  - Specify the `Port` your application is listening on
+
+    ```yaml
+    {
+        "ServiceName": "charris-fasthtmx",
+        "SourceConfiguration": {
+            "AuthenticationConfiguration": {
+                "AccessRoleArn": "arn:aws:iam::500805876079:role/service-role/AppRunnerECRAccessRole"
+            },
+            "AutoDeploymentsEnabled": true,
+            "ImageRepository": {
+                "ImageIdentifier": "500805876079.dkr.ecr.us-east-1.amazonaws.com/charris/fasthtmx:latest",
+                "ImageConfiguration": {
+                    "Port": "8000"
+                },
+                "ImageRepositoryType": "ECR"
+            }
+        },
+        "InstanceConfiguration": {
+            "Cpu": "1 vCPU",
+            "Memory": "3 GB"
+        }
+    }
+    ```
+
+- `aws apprunner create-service --cli-input-json file://aws-apprunner.json`
+
+### View the creation progress
+
+- Use the `ServiceArn` output by the creation step to view the status as the service is created
+
+```bash
+aws apprunner list-operations --service-arn arn:aws:apprunner:us-east-1:500805876079:service/charris-fasthtmx/62b8810a1414433e874ab15483dade21
+```
+
+### Get the URL for your application
+
+- use the `ServiceArn` output by the creation step to view the description of the service, which includes the `ServiceURL`
+
+```bash
+aws apprunner describe-service --service-arn arn:aws:apprunner:us-east-1:500805876079:service/charris-fasthtmx/62b8810a1414433e874ab15483dade21
+```
+
+## 4-Update and re-deploy
+
+- Modify the application (suggestion: increment # in H1 of templates/home/index.html)
+- Update the Docker image
+    `docker build --tag fasthtmx .`
+- Run the local Docker image
+    `docker run --name fasthtmx --publish 8000:8000 fasthtmx`
+- Verify the modification on the local site
+    `http://localhost:8000`
+- Publish the new docker image to ECR
+  - Login to ECR (if necessary)
+
+    ```bash
+    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 500805876079.dkr.ecr.us-east-1.amazonaws.com
+    ```
+
+  - Tag the image
+
+    ```bash
+    docker tag fasthtmx 500805876079.dkr.ecr.us-east-1.amazonaws.com/charris/fasthtmx:latest
+    ```
+
+  - Push the image to ECR
+
+    ```bash
+    docker push 500805876079.dkr.ecr.us-east-1.amazonaws.com/charris/fasthtmx:latest
+    ```
+
+- View deployment progress
+
+    ```bash
+    aws apprunner list-operations --service-arn arn:aws:apprunner:us-east-1:500805876079:service/charris-fasthtmx/62b8810a1414433e874ab15483dade21
+    ```
+
+- Verify the modification on the cloud site
+    [https://8m7sdrbget.us-east-1.awsapprunner.com/](https://8m7sdrbget.us-east-1.awsapprunner.com/)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, Request, Form
+from fastapi import FastAPI, Depends, HTTPException, Request, Form, status
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -31,26 +31,26 @@ def get_db():
         db.close()
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def home_page(request: Request, db: Session = Depends((get_db))):
     vm = homeviewmodel.HomeViewModel(db=db)
     books = vm.books
-    print(f'Request for home_page page received')
-    return templates.TemplateResponse('home/index.html', {"request": request, "books": books})
+    print(f'Request for home_page page received(test1)')
+    return templates.TemplateResponse('home/index.html', {"request": request, "books": books}, status_code=status.HTTP_200_OK)
 
 
-@app.get("/author/add")
+@app.get("/author/add", status_code=status.HTTP_200_OK)
 def authors_add(request: Request):
     print(f'Request to get /author/add page received')
     return templates.TemplateResponse('authors/partials/add_authors_form.html', {"request": request})
 
 
-@app.post("/authors/add")
+@app.post("/authors/add", status_code=status.HTTP_201_CREATED)
 def create_author(request: Request, email: str = Form(...), first_name: str = Form(...), last_name: str = Form(...), db: Session = Depends(get_db)):
     print(f'Request to post /author/add page received with email:{email}, first_name:{first_name}, last_name:{last_name}')
     db_author = dbs.get_author_by_email(db, email=email)
     if db_author:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email already registered")
     author = schema.AuthorCreate(last_name=last_name, first_name=first_name, email=email)
     dbs.create_author(db=db, author=author)
     url = request.headers.get('HX-Current-URL').split('/')[-1]
@@ -60,10 +60,10 @@ def create_author(request: Request, email: str = Form(...), first_name: str = Fo
         return templates.TemplateResponse('books/partials/show_add_form.html', {"request": request})
     else:
         pass
-    return RedirectResponse(url="/", status_code=302)
+    return RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
 
 
-@app.get("/authors/cancel_add")
+@app.get("/authors/cancel_add", status_code=status.HTTP_200_OK)
 def cancel_author(request: Request):
     print(f'Request to get /author/cancel_add page received')
 
@@ -73,13 +73,13 @@ def cancel_author(request: Request):
     return templates.TemplateResponse('books/partials/show_add_form.html', {"request": request})
 
 
-@app.get("/authors/close_books/{author_id}")
+@app.get("/authors/close_books/{author_id}", status_code=status.HTTP_200_OK)
 def close_authors_books(request: Request, author_id: int):
     print(f'Request to get /authors/close_books/ page received with author_id:{author_id}')
     return templates.TemplateResponse('authors/partials/show_books.html', {"request": request, "author_id": author_id})
 
 
-@app.get("/authors")
+@app.get("/authors", status_code=status.HTTP_200_OK)
 def show_authors(request: Request, db: Session = Depends(get_db)):
     print(f'Request to get /authors page received')
     vm = showauthors.ShowAuthorsViewModel(db=db)
@@ -87,7 +87,7 @@ def show_authors(request: Request, db: Session = Depends(get_db)):
     return templates.TemplateResponse('authors/authors.html', {"request": request, "authors": authors})
 
 
-@app.get("/author/books/{author_id}")
+@app.get("/author/books/{author_id}", status_code=status.HTTP_200_OK)
 def authors_books(request: Request, author_id: int, db: Session = Depends(get_db)):
     print(f'Request to get /author/books page received with author_id:{author_id}')
     vm = authorbooks.AuthorBooksViewModel(db=db, author_id=author_id)
@@ -96,7 +96,7 @@ def authors_books(request: Request, author_id: int, db: Session = Depends(get_db
                                       {"request": request, "books": books, "author_id": author_id})
 
 
-@app.get("/books/add")
+@app.get("/books/add", status_code=status.HTTP_200_OK)
 def add_book(request: Request, db: Session = Depends(get_db)):
     print(f'Request to get /books/add page received')
     vm = addbookviewmodel.AddBookViewModel(db=db)
@@ -104,35 +104,35 @@ def add_book(request: Request, db: Session = Depends(get_db)):
     return templates.TemplateResponse('books/partials/add_books_form.html', {"request": request, "data": data})
 
 
-@app.post("/books/add")
+@app.post("/books/add", status_code=status.HTTP_201_CREATED)
 def book_add(title: str = Form(...), pages: str = Form(...), author_id: str = Form(...), db: Session = Depends(get_db)):
     print(f'Request to get /books/add page received with title:{title}, pages:{pages}')
     db_book = dbs.get_book(db, title=title)
     if db_book:
-        raise HTTPException(status_code=400, detail="Book already exists.")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Book already exists.")
     book = schema.CreateBook
     book.title = title
     book.author_id = int(author_id)
     book.pages = int(pages)
     dbs.create_book(db, book=book)
-    return RedirectResponse(url="/", status_code=302)
+    return RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
 
 
-@app.get("/books/cancel_add")
+@app.get("/books/cancel_add", status_code=status.HTTP_200_OK)
 def cancel_add(request: Request):
     print(f'Request to get /books/cancel_add page received')
     return templates.TemplateResponse('books/partials/show_add_form.html', {"request": request})
 
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 def get_books(request: Request, db: Session = Depends(get_db)):
     print(f'Request to get /books page received')
     vm = showbooks.ShowBooksViewModel(db=db)
     books = vm.books
-    return templates.TemplateResponse('books/books.html', {"request": request, "books": books, "search_text": ""})
+    return templates.TemplateResponse('books/books.html', {"request": request, "books": books, "search_text": ""}, status_code=status.HTTP_200_OK)
 
 
-@app.get("/books/search")
+@app.get("/books/search", status_code=status.HTTP_200_OK)
 def search_books(request: Request, search_text: str, db: Session = Depends(get_db)):
     print(f'Request to get /books/search page received with search_text:{search_text}')
     vm = searchbooks.SearchViewModel(db=db, search_text=search_text)
